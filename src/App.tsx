a2a6788665e3aed1d81104fe463a05a7373ac8e2
Julian Lasso <prv_julian.lasso@dale.com.co>
2023-08-09 08:01:05 -0500

    feat: :tada: first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import type {ReactElement} from 'react';
+import React from 'react';
+import {Text} from 'react-native';
+
+function App(): ReactElement {
+  return <Text>hello world</Text>;
+}
+
+export default App;
